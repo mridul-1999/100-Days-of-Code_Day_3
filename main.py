416a1bfd4f86fe0ab69562626f29a87bfd4237d5
Saif Mahmud <67528956+mridul-1999@users.noreply.github.com>
2023-04-04 13:31:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+#Day 3 challange
+
+food = input("Name a Food : ")
+plant = input("Name a Type of plant :")
+cooking = input("Name a method of cooking : ")
+burned_food = input("what word describes burned food : ")
+diy_item = input("Name a DIY item : ")
+
+print()
+print("MENU")
+print(cooking,food,"With",burned_food,plant,"on a bed of",diy_item)
+
+
+
+'''
+#fix my Code
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called",person,"Who did something cool like", thing, "at the wonderful", place ,"where you'll find me", rhyme)
+
+
+
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is", whatYear)
+
+
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName,"thinks it is",whatYear)
+
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing: ")
+
+print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+'''
